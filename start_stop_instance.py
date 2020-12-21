9177cb3e1c6318cb6036c49546c7d8bcff382d8d
Boxing <boxingp@gmail.com>
2020-12-21 15:02:19 +0800

    AL-8: refactor the functions by removing the business logic.

--- start_stop_instance.py
+++ start_stop_instance.py
@@ -1,37 +1,31 @@
-# -*- encoding:utf-8 -*-
 import datetime
 import time
 import boto3
-import yaml
-import collections
-
-from policy import policy
-
-ec2_client = boto3.client('ec2')
-rds_client = boto3.client('rds')
-dry_run = False
 
 
-def check_ec2_status(ec2):
-    response = ec2_client.describe_instances(
-        InstanceIds=[ec2]
-    )
-    return response['Reservations'][0]['Instances'][0]['State']['Code']
-
-
-def check_rds_status(rds):
-    response = rds_client.describe_db_instances(
-        DBInstanceIdentifier=rds
-    )
-    return response['DBInstances'][0]['DBInstanceStatus']
+def check_instance_status(service, instance):
+    try:
+        client = boto3.client(service)
+        if service == 'ec2':
+            response = client.describe_instances(
+                InstanceIds=[instance]
+            )
+            return response['Reservations'][0]['Instances'][0]['State']['Name']
+        elif service == 'rds':
+            response = client.start_db_instance(
+                DBInstanceIdentifier=instance
+            )
+            return response['DBInstances'][0]['DBInstanceStatus']
+        return ''
+    except Exception as e:
+        print('No action taken: %s' % e)
+        return ''
 
 
 def remove_running(service, instances):
     instances_to_operate = instances
     for instance in instances:
-        if service == 'ec2' and check_ec2_status(instance) == 16:
-            instances_to_operate.remove(instance)
-        elif service == 'rds' and check_rds_status(instance) == 'available':
+        if check_instance_status(service, instance) in ['running', 'available']:
             instances_to_operate.remove(instance)
 
     return instances_to_operate
@@ -40,9 +34,7 @@ def remove_running(service, instances):
 def remove_stopped(service, instances):
     instances_to_operate = instances
     for instance in instances:
-        if service == 'ec2' and check_ec2_status(instance) in [80, 64]:
-            instances_to_operate.remove(instance)
-        elif service == 'rds' and check_rds_status(instance) in ['stopped', 'stopping']:
+        if check_instance_status(service, instance) in ['stopping', 'stopped']:
             instances_to_operate.remove(instance)
 
     return instances_to_operate
@@ -57,146 +49,82 @@ def wait_until(condition, timeout, period=5, *args):
     raise ValueError('%s is incorrect.' % condition)
 
 
-def rds_is_on(instances):
+def start_instance(service, instance):
+    response = ''
+    try:
+        print('Starting %s' % instance)
+        client = boto3.client(service)
+        if service == 'ec2':
+            response = client.start_instances(
+                InstanceIds=[instance]
+            )
+        elif service == 'rds':
+            response = client.start_db_instance(
+                DBInstanceIdentifier=instance
+            )
+        return response
+    except Exception as e:
+        print('No action taken: %s' % e)
+        return False
+
+
+def instances_are_on(service, instances):
     for instance in instances:
-        if check_rds_status(instance) != 'available':
+        if check_instance_status(service, instance) not in ['running', 'available']:
             print('%s is still in starting' % instance)
             return False
     return True
 
 
-def ec2_is_off(instances):
+def stop_instance(service, instance):
+    response = ''
+    try:
+        print('Stopping %s' % instance)
+        client = boto3.client(service)
+        if service == 'ec2':
+            response = client.stop_instances(
+                InstanceIds=[instance]
+            )
+        elif service == 'rds':
+            response = client.stop_db_instance(
+                DBInstanceIdentifier=instance
+            )
+        return response
+    except Exception as e:
+        print('No action taken: %s' % e)
+        return False
+
+
+def instances_are_off(service, instances):
     for instance in instances:
-        if check_ec2_status(instance) != 80:
+        if check_instance_status(service, instance) != 'stopped':
             print('%s is still in stopping' % instance)
             return False
     return True
 
 
-def start_instances(ec2_list, rds_list):
-    print('Starting the RDS instances...')
-    rds_list = remove_running('rds', rds_list)
-    print(','.join(rds_list))
-    for rds in rds_list:
-        start_rds_instance(rds)
+def start_instances(service, instances):
+    print('Starting the {} instances...'.format(service))
+    instances = remove_running(service, instances)
+    print(','.join(instances))
+    for instance in instances:
+        start_instance(service, instance)
 
-    wait_until(rds_is_on, 800, 20, rds_list)
-
-    print('Starting the EC2 instances...')
-    ec2_list = remove_running('ec2', ec2_list)
-    print(','.join(ec2_list))
-    for ec2 in ec2_list:
-        start_ec2_instance(ec2)
+    wait_until(instances_are_on, 800, 20, service, instances)
 
 
-def stop_instances(ec2_list, rds_list):
-    print('Stopping the EC2 instances...')
-    ec2_list = remove_stopped('ec2', ec2_list)
-    print(','.join(ec2_list))
-    for ec2 in ec2_list:
-        stop_ec2_instance(ec2)
+def stop_instances(service, instances):
+    print('Stopping the {} instances...'.format(service))
+    instances = remove_stopped('ec2', instances)
+    print(','.join(instances))
+    for instance in instances:
+        stop_instance(service, instance)
 
-    wait_until(ec2_is_off, 800, 20, ec2_list)
-
-    print('Stopping the RDS instances...')
-    rds_list = remove_stopped('rds', rds_list)
-    print(','.join(rds_list))
-    for rds in rds_list:
-        stop_rds_instance(rds)
-
-
-def get_tags_to_filter():
-    with open('tags_filter.yaml', 'r') as tags_file:
-        tags = yaml.load(tags_file, Loader=yaml.FullLoader)
-    return tags['aws_tags']
-
-
-def get_ec2_instances_by_tag(tags):
-    custom_filter = []
-    for tag in tags:
-        for key, value in tag.items():
-            custom_filter.append({'Name': 'tag:' + key, 'Values': [value]}.copy())
-
-    response = ec2_client.describe_instances(
-        Filters=custom_filter
-    )
-
-    ec2_to_operate = []
-    for reservation in response['Reservations']:
-        for instance in reservation['Instances']:
-            ec2_to_operate.append(instance['InstanceId'])
-
-    return ec2_to_operate
-
-
-def get_rds_instances_by_tag(tags):
-    response = rds_client.describe_db_instances()
-    rds_to_operate = []
-
-    for tag in tags:
-        for key, value in tag.items():
-            for instance in response['DBInstances']:
-                for rds_tag in instance['TagList']:
-                    if rds_tag['Key'] == key and rds_tag['Value'] == value:
-                        rds_to_operate.append(instance['DBInstanceIdentifier'])
-                else:
-                    continue
-    rds_to_operate = [item for item, count in collections.Counter(rds_to_operate).items() if count == len(tags)]
-    return rds_to_operate
-
-
-def start_ec2_instance(ec2):
-    try:
-        print('Starting %s' % ec2)
-        response = ec2_client.start_instances(
-            InstanceIds=[ec2],
-            DryRun=dry_run
-        )
-
-    except Exception as e:
-        print('No action taken: %s' % e)
-        return False
-
-    else:
-        return response
-
-
-def start_rds_instance(rds):
-    print('Starting %s' % rds)
-    rds_client.start_db_instance(
-        DBInstanceIdentifier=rds
-    )
-
-
-def stop_ec2_instance(ec2):
-    try:
-        print('Stopping %s' % ec2)
-        response = ec2_client.stop_instances(
-            InstanceIds=[ec2],
-            DryRun=dry_run
-        )
-
-    except Exception as e:
-        print('No action taken: %s' % e)
-        return False
-
-    else:
-        return response
-
-
-def stop_rds_instance(rds):
-    print('Stopping %s' % rds)
-    rds_client.stop_db_instance(
-        DBInstanceIdentifier=rds
-    )
+    wait_until(instances_are_off, 800, 20, service, instances)
 
 
 def lambda_handler(event, context):
-    tags = get_tags_to_filter()
-    ec2_list = get_ec2_instances_by_tag(tags)
-    rds_list = get_rds_instances_by_tag(tags)
-
-    if policy():
-        start_instances(ec2_list, rds_list)
+    if event['is_start']:
+        start_instances(event['instance_type'], event['instance_ids'])
     else:
-        stop_instances(ec2_list, rds_list)
+        stop_instances(event['instance_type'], event['instance_ids'])
--- suspend_scaling.py
+++ suspend_scaling.py
@@ -1,31 +1,4 @@
 import boto3
-import yaml
-
-from policy import policy
-
-
-def get_tags_to_filter():
-    with open('tags_filter.yaml', 'r') as tags_file:
-        tags = yaml.load(tags_file, Loader=yaml.FullLoader)
-    return tags['aws_tags']
-
-
-def get_scaling_group_by_tag(tags):
-    group_list = []
-    filtered_group = []
-    autoscaling_client = boto3.client('autoscaling')
-    paginator = autoscaling_client.get_paginator('describe_auto_scaling_groups')
-    page_iterator = paginator.paginate(
-        PaginationConfig={'PageSize': 100}
-    )
-    group_filter = 'AutoScalingGroups[]'
-    for tag in tags:
-        key, value = list(tag.items())[0]
-        group_filter = ('{} | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format(group_filter, key, value))
-        filtered_group = page_iterator.search(group_filter)
-    for group in filtered_group:
-        group_list.append(group['AutoScalingGroupName'])
-    return group_list
 
 
 def suspend_scaling(group_name):
@@ -44,6 +17,7 @@ def suspend_scaling(group_name):
             'ScheduledActions'
         ]
     )
+    print('{} is suspended.'.format(group_name))
     return response
 
 
@@ -63,16 +37,14 @@ def resume_scaling(group_name):
             'ScheduledActions'
         ]
     )
+    print('{} is resumed.'.format(group_name))
     return response
 
 
 def lambda_handler(event, context):
-    tags = get_tags_to_filter()
-    group_list = get_scaling_group_by_tag(tags)
-
-    if policy():
-        for group in group_list:
+    if event['is_suspend']:
+        for group in event['group_names']:
             suspend_scaling(group)
     else:
-        for group in group_list:
+        for group in event['group_names']:
             resume_scaling(group)
